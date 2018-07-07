5fc46ea7d96541f6de3463eebd3df63471d076e1
Daniel Deutsch <deudan1010@gmail.com>
2018-07-07 12:08:49 +0200

    Cleanup Mnist; add launch.json for debugging

--- mnistBasic.py
+++ mnistBasic.py
@@ -5,7 +5,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 # Only log errors (to prevent unnecessary cluttering of the console)
 tf.logging.set_verbosity(tf.logging.ERROR)
 
-logpath="./tfb_logs/"
+logpath = "./tfb_logs/"
 
 # We use the TF helper function to pull down the data from the MNIST site
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
@@ -19,27 +19,21 @@ y_ = tf.placeholder(tf.float32, shape=[None, 10], name="y_placeholder")
 W = tf.Variable(tf.zeros([784, 10]), name="weights_variable")
 b = tf.Variable(tf.zeros([10]), name="bias_variable")
 
-# Define the activation function. Here softmax for classification
+# Define the activation function = the real y. Softmax suits well for classification
 y = tf.nn.softmax(tf.matmul(x, W) + b, name="softmaxActivation")
 
-print(x, y_, W, b)
-
-# Loss is cross entropy
-cross_entropy = tf.reduce_mean(
-    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y)
+# Loss is defined as cross entropy between the prediction and the real value
+# Each training step in gradient descent we want to minimize the loss
+loss = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_, logits=y, name="lossFunction")
 )
-
-# Each training step in gradient descent we want to minimize cross entropy
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(loss, name="gradDescent")
 
 # Initialize all variables
-init = tf.global_variables_initializer()
-
-# Create an interactive session that can span multiple code blocks. Don't
-# forget to explicity close the session with sess.close()
-sess = tf.Session()
-
+# Create an interactive session that can span multiple code blocks.
 # Perform the initialization which is only the initialization of all global variables
+init = tf.global_variables_initializer()
+sess = tf.Session()
 sess.run(init)
 
 # TensorBoard - Write the default graph out so we can view it's structure
@@ -48,7 +42,6 @@ tbWriter = tf.summary.FileWriter(logpath, sess.graph)
 # Perform 1000 training steps
 for i in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(100)  # get 100 random data points
-
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
 # Evaluate how well the model did. Do this by compating the digit with the highest probability in

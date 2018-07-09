d5a0f8a469671e7f21993cb791b4840ea9e237fe
Daniel Deutsch <deudan1010@gmail.com>
2018-07-09 18:04:08 +0200

    Setup for tensorboard debugger

--- mnistBasic.py
+++ mnistBasic.py
@@ -1,6 +1,7 @@
 
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
+from tensorflow.python import debug as tf_debug
 
 # Only log errors (to prevent unnecessary cluttering of the console)
 tf.logging.set_verbosity(tf.logging.ERROR)
@@ -30,10 +31,16 @@ loss = tf.reduce_mean(
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(loss, name="gradDescent")
 
 # Initialize all variables
+
 # Create an interactive session that can span multiple code blocks.
 # Perform the initialization which is only the initialization of all global variables
 init = tf.global_variables_initializer()
-sess = tf.InteractiveSession()
+
+sess = tf.Session()
+# sess = tf.InteractiveSession()
+# sess = tf_debug.LocalCLIDebugWrapperSession(sess)
+
+sess = tf_debug.TensorBoardDebugWrapperSession(sess, "DanielDeutschs-MacBook-Pro.local:8080")
 sess.run(init)
 
 
@@ -45,15 +52,23 @@ for i in range(1000):
 
 # Evaluate the accuracy of the model
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-print(correct_prediction.eval())
+# correct_prediction = tf.Print(
+#     correct_prediction, [correct_prediction], message="correct_prediction"
+# )
+# correct_prediction.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels})
 
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels})
+
 test_accuracy = sess.run(
     accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}
 )
-print("Test Accuracy: {0}%".format(test_accuracy * 100.0))
+print("Test Accuracy: {}%".format(test_accuracy * 100.0))
 
 sess.close()
 
 # TensorBoard - Write the default graph out so we can view it's structure
 tbWriter = tf.summary.FileWriter(logpath, sess.graph)
+
+
+# tensorboard --logdir tfb_logs --port 8090 --debugger_port 8080 --host 127.0.0.1

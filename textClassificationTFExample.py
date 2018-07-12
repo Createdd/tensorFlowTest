2a864e962b8a595615619bf1e5da5e0ac78a0ccf
Daniel Deutsch <deudan1010@gmail.com>
2018-07-12 15:41:30 +0200

    Improve converting of integers to words

--- textClassificationTFExample.py
+++ textClassificationTFExample.py
@@ -3,43 +3,37 @@
 
 import tensorflow as tf
 from tensorflow import keras
-
 import numpy as np
-
-print(tf.__version__)
 
 # ======= Import the IMDB dataset
 
 imdb = keras.datasets.imdb
-
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
 
 # ======= Explore the data
 
 print("Training entries: {}, labels: {}".format(len(train_data), len(train_labels)))
 print(train_data[0])
-len(train_data[0]), len(train_data[1])
 
 # ======= Convert integers back to words
 
-# A dictionary mapping words to an integer index
+# Retrieves the dictionary mapping word indices back to words.
 word_index = imdb.get_word_index()
 
-# The first indices are reserved
+
+# Add 3 new indices
+# Exchange key and values
+# Create function to create the text according to the indices
 word_index = {k: (v + 3) for k, v in word_index.items()}
 word_index["<PAD>"] = 0
 word_index["<START>"] = 1
-word_index["<UNK>"] = 2  # unknown
+word_index["<UNK>"] = 2
 word_index["<UNUSED>"] = 3
-
 reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
 
 
-def decode_review(text):
-    return " ".join([reverse_word_index.get(i, "?") for i in text])
-
-
-decode_review(train_data[0])
+def decode_review(integerList):
+    return " ".join([reverse_word_index.get(i, "?") for i in integerList])
 
 
 # ====== Prepare data
@@ -52,8 +46,7 @@ test_data = keras.preprocessing.sequence.pad_sequences(
 )
 
 len(train_data[0]), len(train_data[1])
-
-print(train_data[0])
+# print(train_data[0])
 
 # ========== Build the model
 
